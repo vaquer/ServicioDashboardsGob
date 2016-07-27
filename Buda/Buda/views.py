ac620c4181bbc46f612e944a03c462cbcc9b52e3
Frank <akura11.tt@gmail.com>
2016-07-27 13:50:45 -0500

    Validaciones para respuestas

--- Buda/Buda/views.py
+++ Buda/Buda/views.py
@@ -3,11 +3,15 @@ from django.http import JsonResponse
 
 
 def home(request):
-    return JsonResponse(cache.get('resumen-dependendencias'))
+    return JsonResponse(cache.get('resumen-dependendencias', {}))
 
 
 def recursos_mas_descargados(request):
-    recursos = cache.get('descargas-recursos')
-    recursos_ordenados = sorted(recursos, key=recursos.__getitem__, reverse=True)[:5]
+    recursos = cache.get('descargas-recursos', None)
+    respuesta_ordenada = {}
 
-    return JsonResponse({recurso: recursos[recurso] for recurso in recursos_ordenados}, safe=False)
+    if not None:
+		recursos_ordenados = sorted(recursos, key=recursos.__getitem__, reverse=True)[:5]
+		respuesta_ordenada = {recurso: recursos[recurso] for recurso in recursos_ordenados}
+
+    return JsonResponse(respuesta_ordenada, safe=False)
