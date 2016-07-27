8d61e00c030381aa36725b2c1eb2ff03a7f5f485
Frank <akura11.tt@gmail.com>
2016-07-27 13:58:38 -0500

    Validaciones para respuestas

--- Buda/Docker/cron.d
+++ Buda/Docker/cron.d
@@ -1,5 +1,5 @@
 # placed in /etc/cron.d 
 
-0 1 * * * root python /project/Buda/Buda/cron_buda.py >> /var/log/buda.log
-*/1 * * * * root python /project/Buda/Buda/cron_buda.py >> /var/log/buda.log
-*/1 * * * * root python print "Hola" >> /var/log/buda.log
+0 1 * * * python /project/Buda/Buda/cron_buda.py >> /var/log/buda.log
+*/1 * * * * python /project/Buda/Buda/cron_buda.py >> /var/log/buda.log
+*/1 * * * * echo "Hola" >> /var/log/buda.log
